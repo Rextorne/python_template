8b81b3724e8415b4355769c1414ddac26d09d0f4
Rextorne <nicorudolph00@gmail.com>
2024-11-13 10:15:25 +0100

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -6,4 +6,6 @@ def sum(a, b):
     return a + b
 
 def subtract (a, b):
+    if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
+        raise ValueError("Only numbers are allowed")
     return a - b
